264a1c1a39f6b63bff35be46fecda862b3d713e6
Vasu <150792332+vasudxv7603@users.noreply.github.com>
2025-07-23 10:01:00 +0530

    update

--- prompt.py
+++ prompt.py
@@ -1,3 +1,3 @@
-Agent_intro = """Hello Master, Jarvis is now online and ready to assist you!!"""
+Agent_intro = """Hello Master, Jarvis is now online and ready to assist you!"""
 
 Agent_res = """Yes Master, here is what I found for you."""
